8f115a30df2cd840f925ff9c6e8b1dbac7a1ce8b
Amro Moustafa <amro.fahmi@outlook.com>
2019-01-14 21:13:51 +0400

    Add files via upload

---
+++ app/src/main/java/com/example/amromoustafa/myapplication/Language.java
@@ -0,0 +1,120 @@
+package com.example.amromoustafa.myapplication;
+
+public class Language {
+    /**
+     * Arabic.
+     */
+    String ARABIC = "ar";
+
+    /**
+     * Czech.
+     */
+    String CZECH = "cs";
+
+    /**
+     * Danish.
+     */
+    String DANISH = "da";
+
+    /**
+     * German.
+     */
+    String GERMAN = "de";
+
+    /**
+     * Spanish.
+     */
+    String SPANISH = "es";
+
+    /**
+     * English.
+     */
+    String ENGLISH = "en";
+
+    /**
+     * Finnish.
+     */
+    String FINNISH = "fi";
+
+    /**
+     * French.
+     */
+    String FRENCH = "fr";
+
+
+    /**
+     * Hindi.
+     */
+    String HINDI = "hi";
+
+    /**
+     * Hungarian.
+     */
+    String HUNGARIAN = "hu";
+
+    /**
+     * Italian.
+     */
+    String ITALIAN = "it";
+
+    /**
+     * Japanese.
+     */
+    String JAPANESE = "ja";
+
+    /**
+     * Korean.
+     */
+    String KOREAN = "ko";
+
+    /**
+     * Norwegian Bokmal.
+     */
+    String NORWEGIAN_BOKMAL = "nb";
+
+    /**
+     * Dutch.
+     */
+    String DUTCH = "nl";
+
+    /**
+     * Norwegian Nynorsk.
+     */
+    String NORWEGIAN_NYNORSK = "nn";
+
+    /**
+     * Polish.
+     */
+    String POLISH = "pl";
+
+    /**
+     * Portuguese.
+     */
+    String PORTUGUESE = "pt";
+
+    /**
+     * Russian.
+     */
+    String RUSSIAN = "ru";
+
+    /**
+     * Swedish.
+     */
+    String SWEDISH = "sv";
+
+    /**
+     * Turkish.
+     */
+    String TURKISH = "tr";
+
+    /**
+     * Chinese.
+     */
+    String CHINESE = "zh";
+
+    /**
+     * Traditional Chinese.
+     */
+    String TRADITIONAL_CHINESE = "zh-TW";
+
+}
